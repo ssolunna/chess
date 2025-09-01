b5c8a1636df73a1cabe804f60a64930daa9915e2
ssolunna <ssolunna29@gmail.com>
2025-09-01 16:50:44 -0500

    Add 'search legal moves' method for Knight class

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+require_relative '../movements/knight_movement'
+
+# Chess piece: The Knight
+class Knight < Piece
+  include KnightMovement
+
+  def search_legal_moves(board)
+    moves.select { |square| opponent_in_square?(square, board) || empty_square?(square, board) }
+  end
+end
---
+++ spec/pieces/knight_spec.rb
@@ -0,0 +1,168 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/pieces/knight'
+require_relative '../../lib/movements/knight_movement'
+
+RSpec.shared_examples 'a knight' do
+  describe '#search_legal_moves' do
+    context 'when knight is at b1' do
+      context 'if d2 empty and opponent at a3 c3' do
+        it 'returns array of squares: d2, a3, c3' do
+          current_square = 'b1'
+          board = { 'd2' => ' ',
+                    'a3' => double(color: opponent_color),
+                    'c3' => double(color: opponent_color) }
+          expected_array = %w[d2 a3 c3]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when knight is at g1' do
+      context 'if f3, h3 empty and same-color piece at e2' do
+        it 'returns array of squares: f3, h3' do
+          current_square = 'g1'
+          board = { 'e2' => double(color: color),
+                    'f3' => ' ',
+                    'h3' => ' ' }
+          expected_array = %w[f3 h3]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when knight is at b8' do
+      context 'if c6 empty, opponent at a6 and same-color piece at d7' do
+        it 'returns array of squares: a6, c6' do
+          current_square = 'b8'
+          board = { 'd7' => double(color: color),
+                    'c6' => ' ',
+                    'a6' => double(color: opponent_color) }
+          expected_array = %w[a6 c6]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when knight is at g8' do
+      context 'if same-color pieces at h6, f6, e7' do
+        it 'returns an empty array' do
+          current_square = 'g8'
+          board = { 'e7' => double(color: color),
+                    'h6' => double(color: color),
+                    'f6' => double(color: color) }
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to be_empty
+        end
+      end
+    end
+
+    context 'when knight is at e5' do
+      context 'if e6, f5, c4, c6 empty, opponent at d5, f7, f3, d3 and same-color at e4, d7, g6, g4' do
+        it 'returns array of squares: f7, f3, d3, c4, c6' do
+          current_square = 'e5'
+          board = { 'e6' => ' ',
+                    'f5' => ' ',
+                    'e4' => double(color: color),
+                    'd5' => double(color: opponent_color),
+                    'd7' => double(color: color),
+                    'f7' => double(color: opponent_color),
+                    'g6' => double(color: color),
+                    'g4' => double(color: color),
+                    'f3' => double(color: opponent_color),
+                    'd3' => double(color: opponent_color),
+                    'c4' => ' ',
+                    'c6' => ' ' }
+          expected_array = %w[f7 f3 d3 c4 c6]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when knight is at a3' do
+      context 'if b2 empty, opponent at c3, c4, b5 and same-color at b4, b1, c2' do
+        it 'returns array of squares: c4, b5' do
+          current_square = 'a3'
+          board = { 'b2' => ' ',
+                    'c3' => double(color: opponent_color),
+                    'b4' => double(color: color),
+                    'b1' => double(color: color),
+                    'c2' => double(color: color),
+                    'c4' => double(color: opponent_color),
+                    'b5' => double(color: opponent_color) }
+          expected_array = %w[c4 b5]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when knight is at h6' do
+      context 'if g7, f6, g5, g8, f7 empty, opponent at f5, g8 and same-color g4' do
+        it 'returns array of squares: g8, f7, f5' do
+          current_square = 'h6'
+          board = { 'g7' => ' ',
+                    'f6' => ' ',
+                    'g5' => ' ',
+                    'g8' => double(color: opponent_color),
+                    'f7' => ' ',
+                    'f5' => double(color: opponent_color),
+                    'g4' => double(color: color) }
+          expected_array = %w[g8 f7 f5]
+
+          knight = described_class.new(color, current_square)
+          knight.instance_variable_set(:@moves, knight.moves_from(current_square))
+          legal_moves = knight.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+  end
+end
+
+describe Knight do
+  let!(:setup) { KnightMovement.set_up }
+
+  context 'with white knights' do
+    it_behaves_like 'a knight' do
+      let(:color) { 'white' }
+      let(:opponent_color) { 'black' }
+    end
+  end
+
+  context 'with black knights' do
+    it_behaves_like 'a knight' do
+      let(:color) { 'black' }
+      let(:opponent_color) { 'white' }
+    end
+  end
+end
