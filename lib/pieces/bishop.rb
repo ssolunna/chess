aa2a275e1ac802285244958d68b6d9f32e54fe02
ssolunna <ssolunna29@gmail.com>
2025-09-05 15:38:24 -0500

    Add 'search legal moves' method for Bishop class

---
+++ lib/pieces/bishop.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+require_relative '../movements/bishop_movement'
+
+# Chess piece: The Bishop
+class Bishop < Piece
+  include BishopMovement
+
+  def search_legal_moves(board)
+    legal_moves = []
+
+    directions.each do |direction|
+      direction.call(@current_square) do |next_square|
+        if opponent_in_square?(next_square, board)
+          legal_moves << next_square
+          break
+        end
+
+        break unless empty_square?(next_square, board)
+
+        legal_moves << next_square
+      end
+    end
+
+    legal_moves
+  end
+
+  private
+
+  def directions
+    [method(:up_rightward),
+     method(:up_leftward),
+     method(:down_rightward),
+     method(:down_leftward)]
+  end
+end
---
+++ spec/pieces/bishop_spec.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/pieces/bishop'
+require_relative '../../lib/movements/bishop_movement'
+
+RSpec.shared_examples 'a bishop' do
+  describe '#search_legal_moves' do
+    context 'when bishop is at c1' do
+      context 'if a3 d2 e3 g5 h6 empty, same-color at b2 and opponent at f4' do
+        it 'returns array of squares: d2, e3, f4' do
+          current_square = 'c1'
+          board = { 'b2' => double(color: color),
+                    'a3' => ' ',
+                    'd2' => ' ',
+                    'e3' => ' ',
+                    'f4' => double(color: opponent_color),
+                    'g5' => ' ',
+                    'h6' => ' ' }
+          expected_array = %w[d2 e3 f4]
+
+          bishop = described_class.new(color, current_square)
+          legal_moves = bishop.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when bishop is at f1' do
+      context 'if d3 b5 a6 empty, same-color at e2 g2 and opponent at h3 c4' do
+        it 'returns an empty array' do
+          current_square = 'f1'
+          board = { 'g2' => double(color: color),
+                    'h3' => double(color: opponent_color),
+                    'e2' => double(color: color),
+                    'd3' => ' ',
+                    'c4' => double(color: opponent_color),
+                    'b5' => ' ',
+                    'a6' => ' ' }
+
+          bishop = described_class.new(color, current_square)
+          legal_moves = bishop.search_legal_moves(board)
+
+          expect(legal_moves).to be_empty
+        end
+      end
+    end
+
+    context 'when bishop is at c8' do
+      context 'if b7 a6 d7 e6 f5 g4 h3 empty' do
+        it 'returns array of squares: b7, a6, d7, e6, f5, g4, h3' do
+          current_square = 'c8'
+          board = { 'b7' => ' ',
+                    'a6' => ' ',
+                    'd7' => ' ',
+                    'e6' => ' ',
+                    'f5' => ' ',
+                    'g4' => ' ',
+                    'h3' => ' ' }
+          expected_array = %w[b7 a6 d7 e6 f5 g4 h3]
+
+          bishop = described_class.new(color, current_square)
+          legal_moves = bishop.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when bishop is at f8' do
+      context 'if e7 h6 d6 c5 a3 empty, same-color at b4 and opponent at g7' do
+        it 'returns array of squares: g7, e7, d6, c5' do
+          current_square = 'f8'
+          board = { 'g7' => double(color: opponent_color),
+                    'h6' => ' ',
+                    'e7' => ' ',
+                    'd6' => ' ',
+                    'c5' => ' ',
+                    'b4' => double(color: color),
+                    'a3' => ' ' }
+          expected_array = %w[g7 e7 d6 c5]
+
+          bishop = described_class.new(color, current_square)
+          legal_moves = bishop.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+
+    context 'when bishop is at e4' do
+      context 'if f3 c2 h7 d5 c6 empty, same-color at g2 h1 b1 f5
+          and opponent at d3 g6 b7 a8' do
+        it 'returns array of squares: f3, d3, d5, c6, b7' do
+          current_square = 'e4'
+          board = { 'f3' => ' ',
+                    'g2' => double(color: color),
+                    'h1' => double(color: color),
+                    'd3' => double(color: opponent_color),
+                    'c2' => ' ',
+                    'b1' => double(color: color),
+                    'f5' => double(color: color),
+                    'g6' => double(color: opponent_color),
+                    'h7' => ' ',
+                    'd5' => ' ',
+                    'c6' => ' ',
+                    'b7' => double(color: opponent_color),
+                    'a8' => double(color: opponent_color) }
+          expected_array = %w[f3 d3 d5 c6 b7]
+
+          bishop = described_class.new(color, current_square)
+          legal_moves = bishop.search_legal_moves(board)
+
+          expect(legal_moves).to match_array(expected_array)
+        end
+      end
+    end
+  end
+end
+
+describe Bishop do
+  let!(:setup) { BishopMovement.set_up }
+
+  context 'with white bishops' do
+    it_behaves_like 'a bishop' do
+      let(:color) { 'white' }
+      let(:opponent_color) { 'black' }
+    end
+  end
+
+  context 'with black bishops' do
+    it_behaves_like 'a bishop' do
+      let(:color) { 'black' }
+      let(:opponent_color) { 'white' }
+    end
+  end
+end
