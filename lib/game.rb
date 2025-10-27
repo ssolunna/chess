1e1743bed24fa8ed9ce104c43f86577e1b1f40e7
ssolunna <ssolunna29@gmail.com>
2025-10-27 16:37:04 -0500

    Modularize game serialization

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,14 @@
 
 require_relative '../lib/board'
 require_relative '../lib/player'
+require_relative '../lib/serialization'
 
 require 'json'
 
 # Chess Game
 class Game
+  include Serialize
+
   attr_reader :board, :white_player, :black_player,
               :player_in_turn, :winner, :stalemate, :draw,
               :resign, :draw_agreed
@@ -143,12 +146,6 @@ class Game
     @player_in_turn = select_opponent
   end
 
-  def create_piece(piece, color, square)
-    player = color == 'white' ? @white_player : @black_player
-
-    Object.const_get(piece).new(color, square, player)
-  end
-
   def save_game
     File.open(FILENAME, 'w') do |file|
       JSON.dump(serialize_game, file)
@@ -161,81 +158,6 @@ class Game
     deserialize_game(saved_game)
 
     File.delete(FILENAME)
-  end
-
-  def deserialize_game(data)
-    deserialize_board(data['board'])
-    deserialize_player(@white_player, data['white_player'])
-    deserialize_player(@black_player, data['black_player'])
-
-    @player_in_turn = data['player_in_turn'] == 'white' ? @white_player : @black_player
-  end
-
-  def deserialize_board(data)
-    @board.chessboard.each do |square, empty|
-      @board.chessboard[square] = data[square].is_a?(String) ? empty : deserialize_piece(data[square])
-    end
-  end
-
-  def deserialize_piece(data)
-    piece = create_piece(data['name'], data['color'], data['current_square'])
-
-    piece.legal_moves = data['legal_moves']
-
-    piece.moves_log.shift
-    piece.moves_log.push(*data['moves_log'])
-
-    piece
-  end
-
-  def deserialize_player(player, data)
-    data['pieces_moved_log'].each do |data_piece_moved|
-      active_piece = player.pieces.select do |piece|
-        piece.moves_log == data_piece_moved['moves_log']
-      end[0]
-
-      player.pieces_moved_log <<
-        if active_piece.nil?
-          deserialize_piece(data_piece_moved)
-        else
-          active_piece
-        end
-    end
-  end
-
-  def serialize_game
-    {
-      board: serialize_board(@board.chessboard),
-      white_player: serialize_player(@white_player),
-      black_player: serialize_player(@black_player),
-      player_in_turn: @player_in_turn.color
-    }
-  end
-
-  def serialize_board(chessboard)
-    serialized_board = chessboard.dup
-
-    serialized_board.each do |square, value|
-      serialized_board[square] = value.is_a?(String) ? value : serialize_piece(value)
-    end
-
-    serialized_board
-  end
-
-  def serialize_piece(piece)
-    {
-      name: piece.class.to_s,
-      color: piece.color,
-      current_square: piece.current_square,
-      legal_moves: piece.legal_moves,
-      moves_log: piece.moves_log
-    }
-  end
-
-  def serialize_player(player)
-    {
-      pieces_moved_log: player.pieces_moved_log.map { |piece| serialize_piece(piece) }
-    }
   end
 
   def set_winner
@@ -260,7 +182,17 @@ class Game
     @player_in_turn == white_player ? black_player : white_player
   end
 
+  def set_player_in_turn(color)
+    @player_in_turn = color == 'white' ? white_player : black_player
+  end
+
   def default_options
     %w[save resign]
+  end
+
+  def create_piece(piece, color, square)
+    player = color == 'white' ? @white_player : @black_player
+
+    Object.const_get(piece).new(color, square, player)
   end
 end
---
+++ lib/serialization.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module Serialize
+  def serialize_game
+    {
+      board: serialize_chessboard(board.chessboard),
+      white_player: serialize_player(white_player),
+      black_player: serialize_player(black_player),
+      player_in_turn: player_in_turn.color
+    }
+  end
+
+  def serialize_chessboard(chessboard)
+    serialized_board = chessboard.dup
+
+    serialized_board.each do |square, value|
+      serialized_board[square] = value.is_a?(String) ? value : serialize_piece(value)
+    end
+
+    serialized_board
+  end
+
+  def serialize_piece(piece)
+    {
+      name: piece.class.to_s,
+      color: piece.color,
+      current_square: piece.current_square,
+      legal_moves: piece.legal_moves,
+      moves_log: piece.moves_log
+    }
+  end
+
+  def serialize_player(player)
+    {
+      pieces_moved_log: player.pieces_moved_log.map { |piece| serialize_piece(piece) }
+    }
+  end
+
+  def deserialize_game(data)
+    deserialize_board(data['board'])
+    deserialize_player(white_player, data['white_player'])
+    deserialize_player(black_player, data['black_player'])
+
+    set_player_in_turn(data['player_in_turn'])
+  end
+
+  def deserialize_board(data)
+    board.chessboard.each do |square, empty_square|
+      board.chessboard[square] =
+        data[square].is_a?(String) ? empty_square : deserialize_piece(data[square])
+    end
+  end
+
+  def deserialize_piece(data)
+    piece = create_piece(data['name'], data['color'], data['current_square'])
+
+    piece.legal_moves = data['legal_moves']
+
+    piece.moves_log.shift
+    piece.moves_log.push(*data['moves_log'])
+
+    piece
+  end
+
+  def deserialize_player(player, data)
+    data['pieces_moved_log'].each do |data_piece_moved|
+      active_piece = player.pieces.select do |piece|
+        piece.moves_log == data_piece_moved['moves_log']
+      end[0]
+
+      player.pieces_moved_log <<
+        if active_piece.nil?
+          deserialize_piece(data_piece_moved)
+        else
+          active_piece
+        end
+    end
+  end
+end
